158fb0312f64c536f2776101a0eb5a76a09ee9a1
Josef Šimánek <retro@ballgag.cz>
2012-02-27 15:42:43 +0100

    Page seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,49 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# encoding: UTF-8
+if Refinery::Page.where(:menu_match => "^/$").empty?
+  home_page = Refinery::Page.create!({:title => "Karlín Film Fest",
+              :deletable => false,
+              :link_url => "/",
+              :menu_match => "^/$"})
+  home_page.parts.create({
+                :title => "Obsah",
+                :body => "<p>Vítejte na film festu.</p>",
+                :position => 0
+              })
 
-# Added by Refinery CMS Pages engine
-Refinery::Pages::Engine.load_seed
+  page_not_found_page = home_page.children.create(:title => "Stránka neexistuje",
+              :menu_match => "^/404$",
+              :show_in_menu => false,
+              :deletable => false)
+  page_not_found_page.parts.create({
+                :title => "Obsah",
+                :body => "<h2>Požadovaná stránka neexistuje...</h2></p>",
+                :position => 0
+              })
+end
+
+if Refinery::Page.by_title("Soutěžní filmy").empty?
+  about_us_page = ::Refinery::Page.create(:title => "Soutěžní filmy")
+  about_us_page.parts.create({
+                :title => "Obsah",
+                :body => "<p>Krátké info</p>",
+                :position => 0
+              })
+end
+
+if Refinery::Page.by_title("Přidat film").empty?
+  about_us_page = ::Refinery::Page.create(:title => "Přidat film")
+  about_us_page.parts.create({
+                :title => "Obsah",
+                :body => "<p>Krátké info</p>",
+                :position => 0
+              })
+end
+
+if Refinery::Page.by_title("Pravidla").empty?
+  about_us_page = ::Refinery::Page.create(:title => "Pravidla")
+  about_us_page.parts.create({
+                :title => "Obsah",
+                :body => "<p>Krátké info</p>",
+                :position => 0
+              })
+end
