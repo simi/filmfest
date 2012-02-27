f4a4fd8a3db7761a1201a9011492dd7cc3b305d7
Josef Šimánek <retro@ballgag.cz>
2012-02-27 11:35:29 +0100

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FilmFest::Application.config.secret_token = 'a7b43be0fada839e4865665bf9aac8e96bd0112555f8a3e1884ebb6f37deb2eb913332c006e442ea9985fb5de6ca56aeb911ed01be15096d84af0c28039a66b0'
