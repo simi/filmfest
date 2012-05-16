cd3d1efa6387d3030b315a44de90157db7d7b7de
Josef Šimánek <retro@ballgag.cz>
2012-05-16 17:03:23 +0200

    Oprava youtube linku

--- vendor/engines/contest/app/models/refinery/contest/movie.rb
+++ vendor/engines/contest/app/models/refinery/contest/movie.rb
@@ -16,7 +16,7 @@ module Refinery
 
       validates_uniqueness_of :name, :message => 'film s tímto názvem již soutěží'
       validates_format_of :youtube_link,
-        :with => /http:\/\/www.youtube.com\/watch\?v=(.*)&/,
+        :with => /http:\/\/www.youtube.com\/watch\?v=(.*)$/,
         :message => 'Prosím zadejte youtube link ve tvaru "http://www.youtube.com/watch?v=5pr4Ugdg-9k"'
       ["name", "anotation", "year", "director_name", "director_surname",
         "director_year", "screenwriter_name", "screenwriter_surname",
@@ -39,7 +39,7 @@ module Refinery
       end
 
       def youtube_embed
-        link = youtube_link.scan(/http:\/\/www.youtube.com\/watch\?v=(.*)&/).flatten.first
+        link = youtube_link.scan(/http:\/\/www.youtube.com\/watch\?v=(.*)$/).flatten.first
         "<iframe width=\"770\" height=\"400\" src=\"http://www.youtube.com/embed/#{link}?wmode=opaque\" frameborder=\"0\" allowfullscreen></iframe>".html_safe
       end
 
